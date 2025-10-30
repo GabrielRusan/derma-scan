d1d2abe2a04f89c4b98ea97e614076088bf6a4fd
gabisaCSS <gtrmastah@gmail.com>
2025-10-30 13:21:47 +0700

    test update color to green

--- lib/constant/color.dart
+++ lib/constant/color.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart' show Color, Colors;
 
 // Color primary = Color(0xff2486F7);
-Color primary = Color(0xff2486F7);
+Color primary = Colors.green;
 Color secondary = Color(0xff90E0EF);
 Color background = Colors.white;
 Color accent = Color(0xff48CAE4);
